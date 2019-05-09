2e5d86eb8d0d9c473eaea8b716008943de7d6b64
Youssef Youssry <youssef.yossry.2014@gmail.com>
2019-05-09 12:47:57 +0300

    Comment the graph labeling code.

--- src/main/scala/GraphLabeling.scala
+++ src/main/scala/GraphLabeling.scala
@@ -4,14 +4,36 @@ import scala.reflect.ClassTag
 
 class GraphLabeling extends Serializable {
   def run[ED: ClassTag](graph: Graph[Long, ED], maxSteps: Int): VertexRDD[Long] = {
-    def vertexProgram(vid: VertexId, attr: Long, message: Map[VertexId, Long]): VertexId = {
+
+    /*
+    * parameters:
+    *   vid: Id of the vertex
+    *   attr: The current state of the vertex
+    *   message: The aggregated message sent by the neighbouring nodes.
+    * Returns:
+    *   The new state of the vertex
+    * */
+    def vertexProgram(vid: VertexId, attr: Long, message: Map[VertexId, Long]): Long = {
       if (message.isEmpty) attr else message.maxBy(_._2)._1
     }
 
+    /*
+    * parameters:
+    *   e: Edge
+    *
+    * Returns:
+    *   Iterator of the messages that will be sent across this edges.
+    * */
     def sendMessage(e: EdgeTriplet[VertexId, ED]): Iterator[(VertexId, Map[VertexId, Long])] = {
       Iterator((e.srcId, Map(e.dstAttr -> 1L)), (e.dstId, Map(e.srcAttr -> 1L)))
     }
 
+    /*
+    * parameters:
+    *   msgMap1 & msgMap2: Two messages
+    * Returns:
+    *   The merged message.
+    * */
     def mergeMessage(msgMap1: Map[VertexId, Long], msgMap2: Map[VertexId, Long])
     : Map[VertexId, Long] = {
       (msgMap1.keySet ++ msgMap2.keySet).map { key =>
